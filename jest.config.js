ad2506018877b8422d2c7d10c10c302b3bba4929
krasen <krasenparvanov@gmail.com>
2020-11-22 16:49:09 +0100

    #7 Configure jest unit test testing framework for vuejs

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+        'jest-transform-stub',
+    '^.+\\.(js|jsx)?$': '<rootDir>/node_modules/babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: ['jest-serializer-vue'],
+  transformIgnorePatterns: ["/node_modules/(?!(leaflet)/)"],
+ // "collectCoverage": true,
+  //"collectCoverageFrom": ["**/*.{js,vue}", "!**/node_modules/**"]
+};
+
