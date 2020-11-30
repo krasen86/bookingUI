43addbd2b9bfb8187f077a0f99019ffc4ac4b22a
SandraSmolerEisenberg <smolers@student.chalmers.se>
2020-11-30 17:33:32 +0100

    Add booking storage to client
    Issues: helps #9

---
+++ src/store/booking.module.js
@@ -0,0 +1,18 @@
+//import RequestController from '@/services/requestController'
+//import Vue from 'vue'
+
+export const booking = {
+    state:  () => ({
+        booking: {},
+    }),
+    namespaced: true,
+    getters: {
+
+    },
+    actions: {
+
+    },
+    mutations: {
+
+    }
+}
