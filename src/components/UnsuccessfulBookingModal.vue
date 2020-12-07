1db852d0f21cfd32ddb25dac2ff52fbd3b6470db
tammen <gustammjo@student.gu.se>
2020-12-07 11:58:49 +0100

    #12 Add UnsuccessfulBooking to state and adapt display message
    Co-Authored-By: Sandra Smoler Eisenberg <smolers@student.chalmers.se>

--- src/components/UnsuccessfulBookingModal.vue
+++ src/components/UnsuccessfulBookingModal.vue
@@ -7,7 +7,8 @@
         ok-variant="danger"
         @hidden="modalClosed()"
     >
-      <p>Your booking at {{selectedClinic.name}} at {{displayInfo.time}} was unsuccessful!</p>
+      <p>Your booking at {{selectedClinic.name}} on the {{getRequestDate.date}} at {{getRequestDate.time}} was unsuccessful!</p>
+      <p>Please try again!</p>
     </b-modal>
   </div>
 </template>
@@ -35,15 +36,23 @@ export default {
     showModal: {
       // getter
       get: function () {
-        return true; //return this.status === 'received'
+        return this.status === 'unsuccessfulBookingReceived'
       },
       //setter
       set: function() {
       }
     },
-    displayInfo(){
-      return this.booking.response;
+    getRequestDate(){
+      let date = this.booking.request.time;
+      let requestDate = {};
+      date = date.split(" ")
+      requestDate.date = date[0];
+      requestDate.time = date[1];
+
+     return requestDate
     },
+
+
     selectedClinic : {
       get: function () {
         return this.booking.request.dentistid ? this.getClinics.dentists.find(item => item.id === this.booking.request.dentistid) : ""
